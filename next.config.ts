4d7656249d120b087b49bd64fc8b621118efaabc
Rich Allen <allgood2386@gmail.com>
2025-02-27 12:16:22 -0500

    finally config finally uses an env varible

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const RACE_MONITOR_KEY: string = (process.env.REACT_APP_RACE_MONITOR_API_KEY as string);
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    REACT_APP_RACE_MONITOR_API_KEY: RACE_MONITOR_KEY
+  }
 };
 
 export default nextConfig;
